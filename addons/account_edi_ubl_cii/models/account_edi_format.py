cd6092aee3dca5da78e9d4055e418cb0034c52ea
Julien Van Roy <juvr@odoo.com>
2022-09-07 09:39:49 +0000

    [IMP] account_edi_ubl_cii: always generate Factur-X silently
    Always generate a Factur-X xml and embed it when a PDF is created. But no edi.document
    is created, no warnings will be displayed on the invoice, and the PDF is not PDF/A-3.
    The only purpose here is the Odoo interoperability.
    
    Rename the 'Factur-X' option to 'Factur-X PDF/A-3' and make it only visible for FR and DE companies.
    
    This 'Factur-X PDF/A-3' option will trigger the regular account.edi flow: create an edi.document
    and show warnings on the move if errors are detected. When a PDF is printed, embed the Factur-X xml
    and make the PDF a PDF/A-3. Thus, it can be used on Chorus Pro.
    
    task-2957808
    
    closes odoo/odoo#102311
    
    X-original-commit: 25d4635d544a0a48ac937b35c996f2cbdb8073ec
    Related: odoo/enterprise#32344
    Related: odoo/documentation#2789
    Signed-off-by: Laurent Smet <las@odoo.com>
    Signed-off-by: Julien Van Roy <juvr@odoo.com>

--- addons/account_edi_ubl_cii/models/account_edi_format.py
+++ addons/account_edi_ubl_cii/models/account_edi_format.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, _, SUPERUSER_ID
+from odoo import models, fields, _
 from odoo.addons.account_edi_ubl_cii.models.account_edi_common import COUNTRY_EAS
 from odoo.exceptions import UserError
 
@@ -108,7 +108,7 @@ class AccountEdiFormat(models.Model):
 
         builder = self._get_xml_builder(invoice.company_id)
         # For now, the errors are not displayed anywhere, don't want to annoy the user
-        xml_content, _ = builder._export_invoice(invoice)
+        xml_content, errors = builder._export_invoice(invoice)
 
         # DEBUG: send directly to the test platform (the one used by ecosio)
         #response = self.env['account.edi.common']._check_xml_ecosio(invoice, xml_content, builder._export_invoice_ecosio_schematrons())
@@ -124,7 +124,19 @@ class AccountEdiFormat(models.Model):
             attachment_create_vals.update({'res_id': invoice.id, 'res_model': 'account.move'})
 
         attachment = self.env['ir.attachment'].create(attachment_create_vals)
-        return {invoice: {'success': True, 'attachment': attachment}}
+
+        res = {invoice: {'attachment': attachment}}
+        if errors and self.code == 'facturx_1_0_05':
+            res[invoice].update({
+                'success': False,
+                'error': _("Errors occured while creating the EDI document (format: %s). The receiver "
+                           "might refuse it.", builder._description)
+                         + '<p> <li>' + "</li> <li>".join(errors) + '</li> </p>',
+                'blocking_level': 'info',
+            })
+        else:
+            res[invoice]['success'] = True
+        return res
 
     def _get_move_applicability(self, move):
         # EXTENDS account_edi
@@ -152,7 +164,7 @@ class AccountEdiFormat(models.Model):
         if not attachment:
             return
 
-        pdf_writer.embed_odoo_attachment(attachment, subtype='text/xml')
+        pdf_writer.embed_odoo_attachment(edi_document.attachment_id, subtype='text/xml')
         if not pdf_writer.is_pdfa:
             try:
                 pdf_writer.convert_to_pdfa()
