AntashDevani/odoo

[IMP] account,*: manage subsidiary companies
Allow sharing records between company
* accounts
* taxes
* fiscal positions
* products
* ...and some related models

These records can be read and used in children companies.

This can be used to
* have different branding for different businesses
* allow more complex security rules
* consolidate branches differently
* manage different tax reports with different tax ids in the same
  country

task-3371677

closes odoo/odoo#125642

Related: odoo/enterprise#43215
Signed-off-by: Quentin De Paoli <qdp@odoo.com>

addons/account_peppol/models/account_edi_proxy_user.py
@@ -121,10 +121,10 @@ class AccountEdiProxyClientUser(models.Model):
                 # use the first purchase journal if the Peppol journal is not set up
                 # to create the move anyway
                 if not journal_id:
-                    journal_id = self.env['account.journal'].search(
-                        [('company_id', '=', company.id), ('type', '=', 'purchase')],
-                        limit=1,
-                    )
+                    journal_id = self.env['account.journal'].search([
+                        *self.env['account.journal'].check_company_domain(company),
+                        ('type', '=', 'purchase')
+                    ], limit=1)
 
                 attachment_vals = {
                     'name': f'{filename}.xml',

addons/account_peppol/models/res_company.py
@@ -152,10 +152,10 @@ class ResCompany(models.Model):
     def _compute_peppol_purchase_journal_id(self):
         for company in self:
             if company.is_account_peppol_participant and not company.peppol_purchase_journal_id:
-                company.peppol_purchase_journal_id = self.env['account.journal'].search(
-                    [('company_id', '=', company.id), ('type', '=', 'purchase')],
-                    limit=1,
-                )
+                company.peppol_purchase_journal_id = self.env['account.journal'].search([
+                    *self.env['account.journal'].check_company_domain(company),
+                    ('type', '=', 'purchase'),
+                ], limit=1)
             else:
                 company.peppol_purchase_journal_id = False
 

Therp/OCB

addons/account_peppol/models/account_edi_proxy_user.py
@@ -121,10 +121,10 @@ class AccountEdiProxyClientUser(models.Model):
                 # use the first purchase journal if the Peppol journal is not set up
                 # to create the move anyway
                 if not journal_id:
-                    journal_id = self.env['account.journal'].search(
-                        [('company_id', '=', company.id), ('type', '=', 'purchase')],
-                        limit=1,
-                    )
+                    journal_id = self.env['account.journal'].search([
+                        *self.env['account.journal']._check_company_domain(company),
+                        ('type', '=', 'purchase')
+                    ], limit=1)
 
                 attachment_vals = {
                     'name': f'{filename}.xml',

addons/account_peppol/models/res_company.py
@@ -151,10 +151,10 @@ class ResCompany(models.Model):
     def _compute_peppol_purchase_journal_id(self):
         for company in self:
             if company.is_account_peppol_participant and not company.peppol_purchase_journal_id:
-                company.peppol_purchase_journal_id = self.env['account.journal'].search(
-                    [('company_id', '=', company.id), ('type', '=', 'purchase')],
-                    limit=1,
-                )
+                company.peppol_purchase_journal_id = self.env['account.journal'].search([
+                    *self.env['account.journal']._check_company_domain(company),
+                    ('type', '=', 'purchase'),
+                ], limit=1)
             else:
                 company.peppol_purchase_journal_id = False
 