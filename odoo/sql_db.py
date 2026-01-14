4bc1721f40755a61df457354685fe08cd8fe195e
Danny de Jong <ddejong@therp.nl>
2026-01-14 20:22:03 +0100

    [ADD] #12863 Add thread type to log message

--- odoo/sql_db.py
+++ odoo/sql_db.py
@@ -650,9 +650,10 @@ class ConnectionPool(object):
                 _logger.info('%r: Free leaked connection to %r', self, cnx.dsn)
 
         _logger.info(
-            "CONNX %s connections (thread=%s)",
+            "CONNX %s connections (thread=%s, type=%s)",
             len(self._connections),
             str(threading.current_thread().ident),
+            str(getattr(threading.current_thread(), 'type', None)),
         )
         for i, (cnx, used) in enumerate(self._connections):
             if not used and self._dsn_equals(cnx.dsn, connection_info):
